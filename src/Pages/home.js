a8b13e77e31ecd8b0db5526a44398dafcadea1ef
Julian Dans <gr8jld@gmail.com>
2021-02-18 09:53:10 -0800

    homepage styling

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Heading, Text, Container, Center } from '@chakra-ui/react';
+import { Heading, Text, Container, Center, Link } from '@chakra-ui/react';
 export default function Home() {
   return (
     <Container>
       <Center>
-        <Heading as="h1">THE RIFT</Heading>
-      </Center>
+            <Heading as="h1" style={{fontSize: "50px"}}>THE RIFT</Heading>
+            <Text as="i" style={{fontSize: "45px"}}><Heading>A Create Your Own Adventure Story</Heading></Text>
+        </Center>
+        <Center>
+          <Link href="/story/0000" color="teal.500" style={{fontSize: "35px", marginTop: "20px"}}>Begin</Link>
+        </Center>
     </Container>
   );
 }
